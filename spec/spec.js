f0117bf2d0189f4485799e383813855141d81c7a
Giovanni De Toni <giovanni.det@gmail.com>
2016-11-26 17:46:27 +0100

    Add new sample test

--- spec/spec.js
+++ spec/spec.js
@@ -1,7 +1,15 @@
 
 
 describe("TEST", function(){
-	it("to return true", function(done) {	
+	it("to return true", function(done) {
+		var test="";
+		expect(test).toBe("");
+		done();
+	});
+});
+
+describe("TEST2", function(){
+	it("to return true", function(done) {
 		var test="";
 		expect(test).toBe("");
 		done();
